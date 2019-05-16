56bfd5f0663a496723be2409377cf0569d49aaaa
Lợn <henrynguyen202@gmail.com>
2019-05-17 02:02:56 +0700

    update nhieu qua ko nho

---
+++ QLVT/QLVT/Controllers/GaTauController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLVT.Controllers
+{
+    public class GaTauController : Controller
+    {
+        // GET: GaTau
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- QLVT/QLVT/Controllers/HomeController.cs
+++ QLVT/QLVT/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QLVT.Models;
 
 namespace WebApplication1.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private QuanLyVeTauContextEntities db = new QuanLyVeTauContextEntities();
         public ActionResult Index()
         {
             return View();
@@ -26,5 +29,53 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            // Kiểm tra đăng nhập
+            if (Session["user"] == null)
+            {
+                ViewBag.ListError = new List<string>();
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        [HttpPost]
+        public ActionResult Login(string UserName, string Password)
+        {
+            var lsError = new List<string>();
+            var currentUser = db.Users.Where(m => m.UserName == UserName).SingleOrDefault();
+
+            if (currentUser == null)
+            {
+                lsError.Add("Sai tài khoản hoặc mặt khẩu");
+            }
+            else if (currentUser.Password != Password)
+            {
+                lsError.Add("Sai tài khoản hoặc mặt khẩu");
+            }
+
+            // Khi có lỗi
+            if (lsError.Count > 0)
+            {
+                ViewBag.ListError = lsError;
+                return View();
+            }
+            else
+            {
+                Session["user"] = currentUser;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Login", "Home");
+        }
+
     }
 }
